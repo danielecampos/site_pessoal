9617a371080bea47cba75a5fa79d283b2c536af5
Daniele Campos <danielecampos16@gmail.com>
2017-08-20 13:28:32 -0300

    Revert "mudancaodjafasf"
    This reverts commit 0c215ff61c99531288e87e2ea7be7c5fe902659a.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="desenvolvimento de sites e blogs" content="">
     <meta name="Daniele Campos" content="">
     <link rel="icon" href="../../favicon.ico">
-    <title>Daniele Campos || Desenvolvimentos e manutenção de sites e blogs </title>	
+    <title>Daniele Campos | Desenvolvimentos e manutenção de sites e blogs </title>	
 
     <link rel="stylesheet" type="text/css" href="css/style.css">
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet">
